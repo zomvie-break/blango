2a116d73b0d93cbfd2225b111a092a89d12f4f1b
Víctor Vaquero <virtus.mv@gmail.com>
2023-04-27 05:52:29 +0000

    add post detail functionality

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ import blog.views
 from django.urls import path
 
 urlpatterns = [
-    path('', blog.views.index)
+    path('', blog.views.index),
+    path('post/<slug>/', blog.views.post_detail, name='blog_post_detail')
         ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.utils import timezone
 
@@ -7,3 +7,6 @@ def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
   return render(request, 'blog/index.html', {'posts': posts})
 
+def post_detail(request, slug):
+  post = get_object_or_404(Post, slug=slug)
+  return render(request, 'blog/post_detail.html', {'post':post})
