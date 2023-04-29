5aa9280c00e5788599c1108619f83befa7450315
Víctor Vaquero <virtus.mv@gmail.com>
2023-04-29 04:05:26 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -162,6 +162,28 @@ class Dev(Configuration):
 
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "verbose": {
+                "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+                "style": "{",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "verbose",
+            },
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+      }
+
 
 
 class Prod(Dev):
